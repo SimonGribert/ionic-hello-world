4c8b905f022520030261d3917128f4b6e67cc771
Simon Gribert <simon.gribert@live.se>
2022-11-12 19:13:46 +0100

    add p12 file

--- .github/secrets/decrypt_secrets.sh
+++ .github/secrets/decrypt_secrets.sh
@@ -2,18 +2,18 @@
 set -eo pipefail
 
 gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output ./.github/secrets/Ionic_hello_world_test.mobileprovision.mobileprovision ./.github/secrets/Ionic_hello_world_test.mobileprovision.gpg
-gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output ./.github/secrets/ios_distribution.cer ./.github/secrets/ios_dist.cer.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output ./.github/secrets/ios_dev.p12 ./.github/secrets/ios_dev.p12.gpg
 
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 
 cp ./.github/secrets/Ionic_hello_world_test.mobileprovision.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/Ionic_hello_world_test.mobileprovision.mobileprovision
 
 
-security create-keychain -p "$IOS_KEYS" build.keychain
-security import ./.github/secrets/ios_distribution.cer -t agg -k ~/Library/Keychains/build.keychain -P "$IOS_KEYS" -A
+security create-keychain -p "" build.keychain
+security import ./.github/secrets/ios_dev.p12 -t agg -k ~/Library/Keychains/build.keychain -P "" -A
 
 security list-keychains -s ~/Library/Keychains/build.keychain
 security default-keychain -s ~/Library/Keychains/build.keychain
-security unlock-keychain -p "$IOS_KEYS" ~/Library/Keychains/build.keychain
+security unlock-keychain -p "" ~/Library/Keychains/build.keychain
 
 security set-key-partition-list -S apple-tool:,apple: -s -k "" ~/Library/Keychains/build.keychain
